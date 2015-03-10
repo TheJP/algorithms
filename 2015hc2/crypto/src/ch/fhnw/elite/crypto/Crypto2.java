6ba09681dc045b30875e89fe025fdc1aedca1943
thejp <jp_55555@hotmail.com>
2015-03-10 15:38:07 +0100

    Solved crypto 2 and added automatic test

--- 2015hc2/crypto/src/ch/fhnw/elite/crypto/Crypto2.java
+++ 2015hc2/crypto/src/ch/fhnw/elite/crypto/Crypto2.java
@@ -1,16 +1,138 @@
 package ch.fhnw.elite.crypto;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Crypto2 {
+
+	private static final char[] ALPHABET = "_abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+	private static int charIndex(char c) {
+		return (c == '_') ? 0 : c - 'a' + 1;
+	}
+
+	private static char[] getGeneralKey(String plaintext, String cyphertext){
+		char[] result = new char[ALPHABET.length];
+		for(int i = ALPHABET.length - 1; i >= 0; --i){
+			for(int x = plaintext.length() - 1; x >= 0; --x){
+				if(plaintext.charAt(x) == ALPHABET[i]){
+					result[i] = cyphertext.charAt(x);
+				}
+			}
+		}
+		return result;
+	}
+
+	private static char[] sqrtKey(char[] key){
+		//Cycle recognition
+		int[] visited = new int[ALPHABET.length];
+		List<List<Integer>> cycles = new ArrayList<>();
+		int c = 0;
+		for(int i = 0; i < ALPHABET.length; ++i){
+			if(visited[i] == 0){
+				++c;
+				List<Integer> cycle = new ArrayList<>();
+				int x = i;
+				while(visited[x] == 0){
+					visited[x] = c;
+					cycle.add(x);
+					x = charIndex(key[x]); //Iterate
+				}
+				cycles.add(cycle);
+			}
+		}
+		//Distribute cycles to generate the actual key
+		char[] result = new char[ALPHABET.length];
+		int ncycles = cycles.size();
+		while(ncycles > 0){
+			List<Integer> cycle = cycles.remove(--ncycles);
+			int n = cycle.size();
+			if(n % 2 == 0){ //If n % 2 == 0 (n even)
+				//the only possible solution is to combine the cycle with a cycle of equal length
+				int i = 0;
+				while(i < ncycles && cycles.get(i).size() != n){ ++i; }
+				if(i >= ncycles){ throw new IllegalStateException("No solution possible"); }
+				//Found a possible solution:
+				List<Integer> cycle2 = cycles.remove(i); --ncycles;
+				for(i = 0; i < n; ++i){
+					result[cycle.get(i)] = ALPHABET[cycle2.get(i)];
+					result[cycle2.get(i)] = ALPHABET[cycle.get((i+1) % n)];
+				}
+			} else if(n == 1){ //Special case n == 1
+				result[cycle.get(0)] = ALPHABET[cycle.get(0)];
+			} else { //All other cases with n >= 3 && n % 2 == 1
+				int a = 0;
+				int b = ((int)(n / 2)) + 1;
+				while(b < n){
+					result[cycle.get(a)] = ALPHABET[cycle.get(b)];
+					++a;
+					result[cycle.get(b)] = ALPHABET[cycle.get(a)];
+					++b;
+				}
+				result[cycle.get(a)] = ALPHABET[cycle.get(0)];
+			}
+		}
+		return result;
+	}
+
+//Brute force solution:
+//	private static char[] sqrtKey(char[] key){
+//		char[] solution = new char[ALPHABET.length];
+//		if(!findSolution(key, solution, 0)){
+//			throw new IllegalStateException("Not solvable");
+//		}
+//		return solution;
+//	}
+//
+//	private static boolean findSolution(char[] key, char[] solution, int from){
+//		if(from >= ALPHABET.length){ return true; }
+//		char toc = key[from]; //char to place in solution
+//		if(solution[from] != 0 && toc == solution[charIndex(solution[from])]){
+//			//Already solved this char, next char
+//			return findSolution(key, solution, from+1);
+//		} else {
+//			//Find a solution, which works for this char
+//			if(solution[from] != 0){
+//				//1. Step is fixed (find if 2. step is possible)
+//				int x = charIndex(solution[from]);
+//				if(solution[x] != 0){
+//					//current solution is not 0 and is false: this branch is not solvable
+//					return false;
+//				} else {
+//					//Only one possible way to go: Try to solve with this solution
+//					solution[x] = toc;
+//					if(findSolution(key, solution, from+1)){
+//						return true;
+//					} else {
+//						solution[x] = 0; //Restore previous state
+//					}
+//				}
+//			} else {
+//				//1. Step is still open (find a possible 2 step path)
+//				for(int pos = 0; pos < ALPHABET.length; ++pos){
+//					//Try middle char (x) = pos
+//					if(solution[pos] == 0){
+//						if(pos != from || ALPHABET[pos] == toc){
+//							solution[from] = ALPHABET[pos];
+//							solution[pos] = toc;
+//							if(findSolution(key, solution, from+1)){
+//								return true;
+//							}
+//							solution[from] = 0;
+//							solution[pos] = 0;
+//						}
+//					}
+//				}
+//			}
+//		}
+//		return false;
+//	}
+
 
 	// Returns a key such that applying the key twice to encrypt the plaintext
 	// results in the cyphertext.
 	public static String findKey(String plaintext, String cyphertext) {
-		// TODO: implement me
-		return "_abcdefghijklmnopqrstuvwxzy";
+		return new String(sqrtKey(getGeneralKey(plaintext, cyphertext)));
 	}
 }
--- 2015hc2/crypto/src/ch/fhnw/elite/crypto/CryptoLib2.java
+++ 2015hc2/crypto/src/ch/fhnw/elite/crypto/CryptoLib2.java
@@ -5,90 +5,141 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class CryptoLib2 {
 
-  private static final String ALPHABET = "_abcdefghijklmnopqrstuvwxyz";
+	private static final String ALPHABET = "_abcdefghijklmnopqrstuvwxyz";
 
-  private static void check(boolean v) {
-    if (!v) {
-      System.out.println("assertion failed");
-      System.err.println("assertion failed");
-      System.exit(0);
-    }
-  }
+	private static void check(boolean v) {
+		if (!v) {
+			System.out.println("assertion failed");
+			System.err.println("assertion failed");
+			System.exit(0);
+		}
+	}
 
-  private static boolean isValidKey(String k) {
-    char[] elements = k.toCharArray();
-    Arrays.sort(elements);
-    return Arrays.equals(elements, ALPHABET.toCharArray());
-  }
+	private static boolean isValidKey(String k) {
+		char[] elements = k.toCharArray();
+		Arrays.sort(elements);
+		return Arrays.equals(elements, ALPHABET.toCharArray());
+	}
 
-  private static boolean isValidCleartext(String s) {
-    if (!s.matches("[_a-z]+")) return false;
-    for (int i = 0; i < ALPHABET.length(); ++i) {
-      if (s.indexOf(ALPHABET.charAt(i)) < 0) return false;
-    }
-    return true;
-  }
+	private static boolean isValidCleartext(String s) {
+		if (!s.matches("[_a-z]+"))
+			return false;
+		for (int i = 0; i < ALPHABET.length(); ++i) {
+			if (s.indexOf(ALPHABET.charAt(i)) < 0)
+				return false;
+		}
+		return true;
+	}
 
-  private static int charIndex(char c) {
-    return (c == '_') ? 0 : c - 'a' + 1;
-  }
+	private static int charIndex(char c) {
+		return (c == '_') ? 0 : c - 'a' + 1;
+	}
 
-  private static String encrypt(String message, String key) {
-    char[] output = new char[message.length()];
-    for (int i = 0; i < output.length; ++i) {
-      output[i] = key.charAt(charIndex(message.charAt(i)));
-    }
-    return String.valueOf(output);
-  }
+	private static String encrypt(String message, String key) {
+		char[] output = new char[message.length()];
+		for (int i = 0; i < output.length; ++i) {
+			output[i] = key.charAt(charIndex(message.charAt(i)));
+		}
+		return String.valueOf(output);
+	}
 
-  public static void main(String[] args) {
-      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	private static void normalTest(){
+		try(BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("testdata2.in")))) {
+			String cleartext = null, cyphertext = null;
+			try {
+				cleartext = in.readLine();
+				cyphertext = in.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			try {
+				check(in.readLine().isEmpty());
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			check(cyphertext != null);
+			check(cleartext.length() == cyphertext.length());
+			check(isValidCleartext(cleartext));
+			check(isValidCleartext(cyphertext));
 
-      String cleartext = null, cyphertext = null;
-      try {
-          cleartext = in.readLine();
-          cyphertext = in.readLine();
-      } catch (IOException e) {
-          e.printStackTrace();
-          System.exit(1);
-      }
-      while (cleartext != null) {
-          try {
-              check(in.readLine().isEmpty());
-          } catch (IOException e) {
-              e.printStackTrace();
-              System.exit(1);
-          }
-          check(cyphertext != null);
-          check(cleartext.length() == cyphertext.length());
-          check(isValidCleartext(cleartext));
-          check(isValidCleartext(cyphertext));
+			String key = Crypto2.findKey(cleartext, cyphertext);
+			if (!isValidKey(key)) {
+				System.err.println(1);
+				System.out.println("Incorrect");
+				return;
+			}
+			if (!cyphertext.equals(encrypt(encrypt(cleartext, key), key))) {
+				System.err.println(2);
+				System.out.println("Incorrect");
+				return;
+			}
+			System.out.println("CORRECT");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+	}
 
-          String key = Crypto2.findKey(cleartext, cyphertext);
-          if (!isValidKey(key)) {
-              System.err.println(1);
-              System.out.println("Incorrect");
-              return;
-          }
-          if (!cyphertext.equals(encrypt(encrypt(cleartext, key), key))) {
-              System.err.println(2);
-              System.out.println("Incorrect");
-              return;
-          }
+	private static void autoTest(){
+		try(BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("testdata2.in")))) {
+			String cleartext = null, cyphertext = null;
+			try {
+				cleartext = in.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
 
-          try {
-              cleartext = in.readLine();
-              cyphertext = in.readLine();
-          } catch (IOException e) {
-              e.printStackTrace();
-              System.exit(1);
-          }
-      }
+			for(int i = 0; i < 100; ++i){
 
-      System.out.println("Looks pretty good");
-  }
+				//Generate key and cipher
+				List<Character> tmp = new ArrayList<>(ALPHABET.length());
+				ALPHABET.chars().forEach(c -> tmp.add((char)c));
+				Collections.shuffle(tmp);
+				StringBuilder sb = new StringBuilder(ALPHABET.length());
+				tmp.stream().forEach(c -> sb.append(c));
+				String generatedKey = sb.toString();
+				cyphertext = encrypt(encrypt(cleartext, generatedKey), generatedKey);
+				System.out.println(generatedKey);
+
+				check(cyphertext != null);
+				check(cleartext.length() == cyphertext.length());
+				check(isValidCleartext(cleartext));
+				check(isValidCleartext(cyphertext));
+
+				String key = Crypto2.findKey(cleartext, cyphertext);
+				if (!isValidKey(key)) {
+					System.err.println(1);
+					System.out.println("Incorrect");
+					return;
+				}
+				if (!cyphertext.equals(encrypt(encrypt(cleartext, key), key))) {
+					System.err.println(2);
+					System.out.println("Incorrect");
+					return;
+				}
+				System.out.println("CORRECT");
+
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		normalTest();
+		autoTest();
+	}
 }

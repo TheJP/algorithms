4c2173fae0b5c311bb1d64fadb92a914a624c193
TheJP <jp_55555@hotmail.com>
2016-09-19 21:21:18 +0200

    Solved ADDREV

--- spoj/ADDREVAddingReversedNumbers/reversednumbers.cpp
+++ spoj/ADDREVAddingReversedNumbers/reversednumbers.cpp
@@ -2,12 +2,24 @@
 
 using namespace std;
 
+int reverse(int x) {
+    int y = 0;
+    while (x % 10 == 0 && x > 0) { x /= 10; }
+    while (x > 0) {
+        y *= 10;
+        y += x % 10;
+        x /= 10;
+    }
+    return y;
+}
+
 int main(void) {
     int n, a, b;
     cin >> n;
 
     for (int i = 0; i < n; ++i) {
         cin >> a >> b;
+        cout << reverse(reverse(a) + reverse(b)) << endl;
     }
 
     return 0;
